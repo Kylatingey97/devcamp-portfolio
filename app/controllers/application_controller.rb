e1e26c87e2bfd3ae7db081b32bc6cd188e104c61
Kyla  Tingey <kylatingey@Kylas-MacBook-Pro.local>
2017-01-11 10:26:52 -0700

    Installed devise gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :set_source
+
+  def set_source
+    session[:source] = params[:q] if params[:q]
+  end
 end
