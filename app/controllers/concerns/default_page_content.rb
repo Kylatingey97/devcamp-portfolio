5159b7eb51ab6e4bc5c3248022331fc10357a8d8
Kyla  Tingey <kylatingey@Kylas-MacBook-Pro.local>
2017-01-13 12:20:36 -0700

    Added nav bar and added default header content

---
+++ app/controllers/concerns/default_page_content.rb
@@ -0,0 +1,11 @@
+module DefaultPageContent
+  extend ActiveSupport::Concern
+  included do
+  before_filter :set_page_defaults
+end
+
+def set_page_defaults
+  @page_title = "DevcampPortfolio | My Portfolio Website"
+  @seo_keywords = "Kyla Tingey Portfolio"
+  end
+end
