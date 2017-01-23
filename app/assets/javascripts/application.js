adab834d07e0c7105bfe9c62e4094f12a0f75fd7
Kyla  Tingey <kylatingey@Kylas-MacBook-Pro.local>
2017-01-23 09:59:26 -0700

    Installed jquery ui, sortable html5 library, and css calls

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,9 @@
 // about supported directives.
 //
 //= require jquery
+//= require bootstrap-sprockets
 //= require jquery_ujs
+//= require jquery-ui
+//= require html.sortable
 //= require turbolinks
 //= require_tree .
-//= require bootstrap
